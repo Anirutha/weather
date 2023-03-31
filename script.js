2f0307683c8fac37355e81dad8227f76c98e2497
Anirutha <107671900+Anirutha@users.noreply.github.com>
2023-03-31 18:10:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ res.then((data)=>data.json())
 
 
 function foo(data1){
-    console.log(data1);
+    //console.log(data1);
     for(var i=0;i<data1.length;i++){
+        try{
+        console.log(`Latitude:${data1[i].latlng[0]} Longitude:${data1[i].latlng[1]}`);
+        weatherdata(data1[i].latlng[0],data1[i].latlng[1]);
         row.innerHTML+=
         `<div class="col-md-4">
         <div class="card border-primary mb-3" style="max-width: 18rem;">
@@ -22,44 +25,37 @@ function foo(data1){
         <p class="card-text">Capital : ${data1[i].capital}</p>
         <p class="card-text">Region : ${data1[i].region}</p>
         <p class="card-text">Country Code : ${data1[i].alpha3Code}</p>
-        <button type="button" class="btn btn-primary" click=getdata()>Click for weather</button>
+        <button type="button" class="btn btn-primary" onclick=weatherdata()>Click for weather</button>
+        <p id="weather"></p>
         </div>
         </div>
         </div>
         `
     }
-     document.body.append(container);
-}
-//Multiple api with async and await
-async function getdata(){
-    var res=await fetch("https://restcountries.com/v2/all");
-    var res1= await res.json();
-
-    for(var i=0;i<res1.length;i++){
-        try {
-            // console.log(`Latitude:${res1[i].latlng[0]} Longitude:${res1[i].latlng[1]}`);
-            weatherdata(res1[i].latlng[0],res1[i].latlng[1]);
-        } 
-   catch (error) {
+catch (error) {
     console.log(error);
   }
 }
+     document.body.append(container);
 }
+
+   
 
 async function weatherdata(lat,lon){
 try {
     if(lon===undefined) throw new Error("Invalid Coordinates");
-    //console.log(lat,lon);
-    let res2=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b14ae2d99b3de0b932fa7939fd08f193`);
+    console.log(lat,lon);
+    let res2=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9ef1cec4c7fdd036777255d6bc0463b0`);
     let res3=await res2.json();
-    console.log(`${res3.main.temp}`);
+    document.getElementById("weather").innerHTML=`${res3.main.temp}`;
+    //console.log(res3);
 } catch (error) {
    console.log(error) 
 }
 
 }
 
-getdata();
+weatherdata(lat,lon);
 
 
 
